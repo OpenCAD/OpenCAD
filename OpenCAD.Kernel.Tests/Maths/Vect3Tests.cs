5728a45278739bbbc9e36eab61e1b3bb2f1ef532
veggielane <chris@lane.org.uk>
2014-06-10 19:16:06 +0100

    Added First Files

---
+++ OpenCAD.Kernel.Tests/Maths/Vect3Tests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenCAD.Kernel.Maths;
+
+namespace OpenCAD.Kernel.Tests.Maths
+{
+    [TestClass]
+    public class Vect3Tests
+    {
+        [TestMethod]
+        public void CreationFromDoubles()
+        {
+            var x = 2.0;
+            var y = 3.0;
+            var z = 5.0;
+            var v = new Vect3(x, y, z);
+            Assert.AreEqual(v.X, x, 0.0000001);
+            Assert.AreEqual(v.Y, y, 0.0000001);
+            Assert.AreEqual(v.Z, z, 0.0000001);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof (ArgumentException))]
+        public void CreationFromListTooLong()
+        {
+            var v = new Vect3(new[] {2.0, 3.0, 5.0, 7.0});
+        }
+
+        [TestMethod]
+        public void CreationFromList()
+        {
+            var x = 2.0;
+            var y = 3.0;
+            var z = 5.0;
+            var v = new Vect3(new[] { x, y, z });
+            Assert.AreEqual(v.X, x, 0.0000001);
+            Assert.AreEqual(v.Y, y, 0.0000001);
+            Assert.AreEqual(v.Z, z, 0.0000001);
+        }
+    }
+}
+
---
+++ OpenCAD.Kernel/Maths/Vect3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenCAD.Kernel.Maths
+{
+    public class Vect3
+    {
+        public double X { get; private set; }
+        public double Y { get; private set; }
+        public double Z { get; private set; }
+
+        public Vect3(double x, double y, double z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+        public Vect3(IList<double> a)
+        {
+            if (a.Count != 3) throw new ArgumentException("Array should be double[3]");
+            X = a[0];
+            Y = a[1];
+            Z = a[2];
+        }
+
+    }
+}
