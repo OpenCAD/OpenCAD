2d90722cb29a845f971f22a29c4d1ea21d3ae0c9
veggielane <chris@lane.org.uk>
2014-06-15 16:37:14 +0100

    Renamed tests

---
+++ OpenCAD.Kernel.Tests/Maths/VectorTests.cs
@@ -0,0 +1,314 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenCAD.Kernel.Maths;
+
+namespace OpenCAD.Kernel.Tests.Maths
+{
+    [TestClass]
+    public class VectorTests
+    {
+        private static double Delta = 0.0000001;
+
+        [TestMethod]
+        public void CreationFromDoubles()
+        {
+            var x = 2.0;
+            var y = 3.0;
+            var z = 5.0;
+            var w = 7.0;
+
+            var v2 = new Vect2(x, y);
+            Assert.AreEqual(x, v2.X, Delta);
+            Assert.AreEqual(y, v2.Y, Delta);
+
+            var v3 = new Vect3(x, y, z);
+            Assert.AreEqual(x, v3.X, Delta);
+            Assert.AreEqual(y, v3.Y, Delta);
+            Assert.AreEqual(z, v3.Z, Delta);
+
+            var v4 = new Vect4(x, y, z,w);
+            Assert.AreEqual(x, v4.X, Delta);
+            Assert.AreEqual(y, v4.Y, Delta);
+            Assert.AreEqual(z, v4.Z, Delta);
+            Assert.AreEqual(w, v4.W, Delta);
+        }
+
+        [TestMethod]
+        public void CreationFromListTooLong()
+        {
+            try
+            {
+                var v = new Vect2(new[] {2.0, 3.0, 5.0});
+                Assert.Fail(); // If it gets to this line, no exception was thrown
+            }
+            catch (ArgumentException)
+            {
+
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
+            try
+            {
+                var v = new Vect3(new[] { 2.0, 3.0, 5.0, 7.0 });
+                Assert.Fail(); // If it gets to this line, no exception was thrown
+            }
+            catch (ArgumentException)
+            {
+
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+
+            try
+            {
+                var v = new Vect4(new[] { 2.0, 3.0, 5.0, 7.0, 7.0 });
+                Assert.Fail(); // If it gets to this line, no exception was thrown
+            }
+            catch (ArgumentException)
+            {
+
+            }
+            catch (Exception)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void CreationFromList()
+        {
+            var x = 2.0;
+            var y = 3.0;
+            var z = 5.0;
+            var w = 7.0;
+
+            var v2 = new Vect2(new[] { x, y});
+            Assert.AreEqual(x, v2.X, Delta);
+            Assert.AreEqual(y, v2.Y, Delta);
+
+            var v3 = new Vect3(new[] {x, y, z});
+            Assert.AreEqual(x, v3.X, Delta);
+            Assert.AreEqual(y, v3.Y, Delta);
+            Assert.AreEqual(z, v3.Z, Delta);
+
+            var v4 = new Vect4(new[] { x, y, z, w });
+            Assert.AreEqual(x, v4.X, Delta);
+            Assert.AreEqual(y, v4.Y, Delta);
+            Assert.AreEqual(z, v4.Z, Delta);
+            Assert.AreEqual(w, v4.W, Delta);
+        }
+
+        [TestMethod]
+        public void LengthResult()
+        {
+            var v2 = new Vect2(2.0, 3.0);
+            Assert.AreEqual(13.0, v2.LengthSquared, Delta);
+            Assert.AreEqual(3.60555127546, v2.Length, Delta);
+
+            var v3 = new Vect3(2.0, 3.0, 5.0);
+            Assert.AreEqual(38, v3.LengthSquared, Delta);
+            Assert.AreEqual(6.16441400297, v3.Length, Delta);
+
+            var v4 = new Vect4(2.0, 3.0, 5.0, 7.0);
+            Assert.AreEqual(87, v4.LengthSquared, Delta);
+            Assert.AreEqual(9.32737905308, v4.Length, Delta);
+        }
+
+        [TestMethod]
+        public void NormalisedResult()
+        {
+            Assert.AreEqual(new Vect2(0.5547, 0.83205), new Vect2(2.0, 3.0).Normalized());
+            Assert.AreEqual(new Vect3(0.324443, 0.486664, 0.811107), new Vect3(2.0, 3.0, 5.0).Normalized());
+            Assert.AreEqual(new Vect4(0.214423, 0.321634, 0.536056, 0.750479), new Vect4(2.0, 3.0, 5.0, 7.0).Normalized());
+        }
+
+        [TestMethod]
+        public void StaticDefaults()
+        {
+            Assert.AreEqual(new Vect2(0.0, 0.0), Vect2.Zero);
+            Assert.AreEqual(new Vect2(1.0, 0.0), Vect2.UnitX);
+            Assert.AreEqual(new Vect2(0.0, 1.0), Vect2.UnitY);
+
+            Assert.AreEqual(new Vect3(0.0, 0.0, 0.0), Vect3.Zero);
+            Assert.AreEqual(new Vect3(1.0, 0.0, 0.0), Vect3.UnitX);
+            Assert.AreEqual(new Vect3(0.0, 1.0, 0.0), Vect3.UnitY);
+            Assert.AreEqual(new Vect3(0.0, 0.0, 1.0), Vect3.UnitZ);
+
+            Assert.AreEqual(new Vect4(0.0, 0.0, 0.0, 0.0), Vect4.Zero);
+            Assert.AreEqual(new Vect4(1.0, 0.0, 0.0, 0.0), Vect4.UnitX);
+            Assert.AreEqual(new Vect4(0.0, 1.0, 0.0, 0.0), Vect4.UnitY);
+            Assert.AreEqual(new Vect4(0.0, 0.0, 1.0, 0.0), Vect4.UnitZ);
+            Assert.AreEqual(new Vect4(0.0, 0.0, 0.0, 1.0), Vect4.UnitW);
+        }
+
+        [TestMethod]
+        public void DotProduct()
+        {
+            Assert.AreEqual(14, new Vect2(1.0, 2.0).DotProduct(new Vect2(4.0, 5.0)), Delta);
+            Assert.AreEqual(32, new Vect3(1.0, 2.0, 3.0).DotProduct(new Vect3(4.0, 5.0, 6.0)), Delta);
+            Assert.AreEqual(60, new Vect4(1.0, 2.0, 3.0, 4.0).DotProduct(new Vect4(4.0, 5.0, 6.0, 7.0)), Delta);
+
+            Assert.AreEqual(0, Vect2.UnitX.DotProduct(Vect2.UnitY));
+            Assert.AreEqual(0, Vect3.UnitX.DotProduct(Vect3.UnitY));
+            Assert.AreEqual(0, Vect4.UnitX.DotProduct(Vect4.UnitY));
+        }
+
+        [TestMethod]
+        public void CrossProduct()
+        {
+            Assert.AreEqual(new Vect3(-15.0, -2.0, 39.0), new Vect3(3.0, -3.0, 1.0).CrossProduct(new Vect3(4.0, 9.0, 2.0)));
+            //same vector should equal zero
+            Assert.AreEqual(new Vect3(0, 0, 0), new Vect3(3.0, -3.0, 1.0).CrossProduct(new Vect3(3.0, -3.0, 1.0)));
+            //parallel shuold also equal zero
+            Assert.AreEqual(new Vect3(0, 0, 0), new Vect3(0, 0, 10.0).CrossProduct(new Vect3(0, 0, -10.0)));
+        }
+
+
+
+        [TestMethod]
+        public void Addition()
+        {
+            Assert.AreEqual(new Vect2(7.0, 15.0), new Vect2(3.0, 6.0) + new Vect2(4.0, 9.0));
+            Assert.AreEqual(new Vect2(1.0, 15.0), new Vect2(-3.0, 6.0) + new Vect2(4.0, 9.0));
+
+            Assert.AreEqual(new Vect3(7.0, 15.0, 11.0), new Vect3(3.0, 6.0, 9.0) + new Vect3(4.0, 9.0, 2.0));
+            Assert.AreEqual(new Vect3(1.0, 15.0, 0.0), new Vect3(-3.0, 6.0, 2.0) + new Vect3(4.0, 9.0, -2.0));
+
+            Assert.AreEqual(new Vect4(7.0, 15.0, 11.0, 10.0), new Vect4(3.0, 6.0, 9.0, 7.0) + new Vect4(4.0, 9.0, 2.0, 3.0));
+            Assert.AreEqual(new Vect4(1.0, 15.0, 0.0, 7.0), new Vect4(-3.0, 6.0, 2.0, 5.0) + new Vect4(4.0, 9.0, -2.0, 2.0));
+
+        }
+        [TestMethod]
+        public void Subtract()
+        {
+            Assert.AreEqual(new Vect2(-1.0, -3.0), new Vect2(3.0, 6.0) - new Vect2(4.0, 9.0));
+            Assert.AreEqual(new Vect2(-7.0, -3.0), new Vect2(-3.0, 6.0) - new Vect2(4.0, 9.0));
+
+            Assert.AreEqual(new Vect3(-1.0, -3.0, 7.0), new Vect3(3.0, 6.0, 9.0) - new Vect3(4.0, 9.0, 2.0));
+            Assert.AreEqual(new Vect3(-7.0, -3.0, 4.0), new Vect3(-3.0, 6.0, 2.0) - new Vect3(4.0, 9.0, -2.0));
+
+            Assert.AreEqual(new Vect4(-1.0, -3.0, 7.0, 6.0), new Vect4(3.0, 6.0, 9.0, 12.0) - new Vect4(4.0, 9.0, 2.0, 6.0));
+            Assert.AreEqual(new Vect4(-7.0, -3.0, 4.0, 7.0), new Vect4(-3.0, 6.0, 2.0, 0) - new Vect4(4.0, 9.0, -2.0, -7.0));
+
+
+            Assert.AreEqual(new Vect2(-4.0, -9.0), - new Vect2(4.0, 9.0));
+            Assert.AreEqual(new Vect3(-4.0, -9.0, 2.0), - new Vect3(4.0, 9.0, -2.0));
+            Assert.AreEqual(new Vect4(-4.0, -9.0, 2.0, 6.0), - new Vect4(4.0, 9.0, -2.0, -6.0));
+        }
+
+        [TestMethod]
+        public void Multiply()
+        {
+            Assert.AreEqual(new Vect2(16.0, 36.0), 4.0 * new Vect2(4.0, 9.0));
+            Assert.AreEqual(new Vect2(-8.0, -18.0), -2.0 * new Vect2(4.0, 9.0));
+            Assert.AreEqual(new Vect2(16.0, 36.0), new Vect2(4.0, 9.0) * 4.0);
+            Assert.AreEqual(new Vect2(-8.0, -18.0), new Vect2(4.0, 9.0) * -2.0);
+
+            Assert.AreEqual(new Vect3(16.0, 36.0, 8.0), 4.0 * new Vect3(4.0, 9.0, 2.0));
+            Assert.AreEqual(new Vect3(-8.0, -18.0, 4.0), -2.0 * new Vect3(4.0, 9.0, -2.0));
+            Assert.AreEqual(new Vect3(16.0, 36.0, 8.0), new Vect3(4.0, 9.0, 2.0) * 4.0);
+            Assert.AreEqual(new Vect3(-8.0, -18.0, 4.0), new Vect3(4.0, 9.0, -2.0) * -2.0);
+
+            Assert.AreEqual(new Vect4(16.0, 36.0, 8.0 , 8.0), 4.0 * new Vect4(4.0, 9.0, 2.0, 2.0));
+            Assert.AreEqual(new Vect4(-8.0, -18.0, 4.0, 8.0), -2.0 * new Vect4(4.0, 9.0, -2.0, -4.0));
+            Assert.AreEqual(new Vect4(16.0, 36.0, 8.0, 8.0), new Vect4(4.0, 9.0, 2.0, 2.0) * 4.0);
+            Assert.AreEqual(new Vect4(-8.0, -18.0, 4.0, 8.0), new Vect4(4.0, 9.0, -2.0, -4.0) * -2.0);
+        }
+
+        [TestMethod]
+        public void Divide()
+        {
+            Assert.AreEqual(new Vect2(2.0, 4.5), new Vect2(4.0, 9.0) /2);
+            Assert.AreEqual(new Vect3(2.0, 4.5, 1.0), new Vect3(4.0, 9.0, 2.0) /2);
+            Assert.AreEqual(new Vect4(2.0, 4.5, 1.0,32.0), new Vect4(4.0, 9.0, 2.0, 64.0) /2);
+        }
+
+        [TestMethod]
+        public void Vect2Equality()
+        {
+            var a = new Vect2(2.0, 3.0);
+            var b = new Vect2(2.0, 3.0);
+            var c = new Vect2(3.0, 4.0);
+            var d = a;
+
+            Assert.IsTrue(a.Equals(b));
+            Assert.IsFalse(a.Equals(c));
+            Assert.IsTrue(a.Equals(d));
+
+            Assert.AreEqual(a, b);
+            Assert.AreNotEqual(a, c);
+            Assert.AreEqual(a, d);
+
+            Assert.IsTrue(a == b);
+            Assert.IsTrue(a != c);
+
+            Assert.IsTrue(a != null);
+            Assert.IsTrue(a == a);
+        }
+
+        [TestMethod]
+        public void Vect3Equality()
+        {
+            var a = new Vect3(2.0, 3.0, 5.0);
+            var b = new Vect3(2.0, 3.0, 5.0);
+            var c = new Vect3(3.0, 3.0, 6.0);
+            var d = a;
+
+            Assert.IsTrue(a.Equals(b));
+            Assert.IsFalse(a.Equals(c));
+            Assert.IsTrue(a.Equals(d));
+
+            Assert.AreEqual(a, b);
+            Assert.AreNotEqual(a,c);
+            Assert.AreEqual(a, d);
+
+            Assert.IsTrue(a == b);
+            Assert.IsTrue(a != c);
+
+            Assert.IsTrue(a != null);
+            Assert.IsTrue(a == a);
+        }
+
+        [TestMethod]
+        public void Vect4Equality()
+        {
+            var a = new Vect4(2.0, 3.0, 5.0, 6.0);
+            var b = new Vect4(2.0, 3.0, 5.0, 6.0);
+            var c = new Vect4(3.0, 3.0, 6.0, 7.0);
+            var d = a;
+
+            Assert.IsTrue(a.Equals(b));
+            Assert.IsFalse(a.Equals(c));
+            Assert.IsTrue(a.Equals(d));
+
+            Assert.AreEqual(a, b);
+            Assert.AreNotEqual(a, c);
+            Assert.AreEqual(a, d);
+
+            Assert.IsTrue(a == b);
+            Assert.IsTrue(a != c);
+
+            Assert.IsTrue(a != null);
+            Assert.IsTrue(a == a);
+        }
+
+
+
+        [TestMethod]
+        public void Lerp()
+        {
+            Assert.AreEqual(new Vect3(2.0, 4.5, 1.0), new Vect3(2.0, 4.5, 1.0).Lerp(new Vect3(4.0, 9.0, 2.0), 0.0));
+            Assert.AreEqual(new Vect3(4.0, 9.0, 2.0), new Vect3(2.0, 4.5, 1.0).Lerp(new Vect3(4.0, 9.0, 2.0), 1.0));
+            Assert.AreEqual(new Vect3(0.0, 5.0, 0.0), new Vect3(0.0, 0.0, 0.0).Lerp(new Vect3(0.0, 10.0, 0.0), 0.5));
+            Assert.AreEqual(new Vect3(0.0, 0.0, 0.0), new Vect3(0.0, -10.0, 0.0).Lerp(new Vect3(0.0, 10.0, 0.0), 0.5));
+        }
+
+    }
+}
+
--- OpenCAD.Kernel/Maths/Vect4.cs
+++ OpenCAD.Kernel/Maths/Vect4.cs
@@ -24,11 +24,153 @@ namespace OpenCAD.Kernel.Maths
         }
         public Vect4(IList<double> a)
         {
-            if (a.Count != 2) throw new ArgumentException("Array should be double[2]");
+            if (a.Count != 4) throw new ArgumentException("Array should be double[2]");
             X = a[0];
             Y = a[1];
             Z = a[2];
             W = a[3];
         }
+        
+        public double Length
+        {
+            get { return Math.Sqrt(LengthSquared); }
+        }
+
+        public double LengthSquared
+        {
+            get { return X * X + Y * Y + Z * Z + W * W; }
+        }
+
+        public Vect4 Normalized()
+        {
+            var num = 1f / Length;
+            return new Vect4(X * num, Y * num, Z * num, W * num);
+        }
+
+        public static Vect4 Zero
+        {
+            get { return new Vect4(0.0, 0.0, 0.0, 0.0); }
+        }
+        public static Vect4 UnitX
+        {
+            get { return new Vect4(1.0, 0.0, 0.0, 0.0); }
+        }
+
+        public static Vect4 UnitY
+        {
+            get { return new Vect4(0.0, 1.0, 0.0, 0.0); }
+        }
+
+        public static Vect4 UnitZ
+        {
+            get { return new Vect4(0.0, 0.0, 1.0, 0.0); }
+        }
+
+        public static Vect4 UnitW
+        {
+            get { return new Vect4(0.0, 0.0, 0.0, 1.0); }
+        }
+
+        public double DotProduct(Vect4 v)
+        {
+            return X * v.X + Y * v.Y + Z * v.Z + W * v.W;
+        }
+
+        public Vect4 ExteriorProduct(Vect4 v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static Vect4 operator +(Vect4 v1, Vect4 v2)
+        {
+            return new Vect4(v1.X + v2.X, v1.Y + v2.Y, v1.Z + v2.Z, v1.W + v2.W);
+        }
+
+        public static Vect4 operator -(Vect4 v1, Vect4 v2)
+        {
+            return new Vect4(v1.X - v2.X, v1.Y - v2.Y, v1.Z - v2.Z, v1.W - v2.W);
+        }
+        public static Vect4 operator -(Vect4 v)
+        {
+            return new Vect4(-v.X, -v.Y, -v.Z, -v.W);
+        }
+
+        public static Vect4 operator *(Vect4 v, double d)
+        {
+            return new Vect4(v.X * d, v.Y * d, v.Z * d, v.W * d);
+        }
+
+        public static Vect4 operator *(double d, Vect4 v)
+        {
+            return v * d;
+        }
+
+        public static Vect4 operator /(Vect4 v, double d)
+        {
+            return new Vect4(v.X / d, v.Y / d, v.Z / d, v.W / d);
+        }
+        /// <summary>
+        /// Test for equality
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool operator ==(Vect4 a, Vect4 b)
+        {
+            if (ReferenceEquals(a, b))
+            {
+                return true;
+            }
+            if (((object)a == null) || ((object)b == null))
+            {
+                return false;
+            }
+            return a.Equals(b);
+        }
+
+        /// <summary>
+        /// Test for not equality
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool operator !=(Vect4 a, Vect4 b)
+        {
+            return !(a == b);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj.GetType() == GetType() && Equals((Vect4)obj);
+        }
+
+        protected bool Equals(Vect4 other)
+        {
+            return X.NearlyEquals(other.X) && Y.NearlyEquals(other.Y) && Z.NearlyEquals(other.Z) && W.NearlyEquals(other.W);
+        }
+
+        public override int GetHashCode()
+        {
+            return X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode() ^ W.GetHashCode();
+        }
+
+ 
+        public Vect4 Lerp(Vect4 end, double t)
+        {
+            return (1 - t) * this + t * end;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Vector4({0},{1},{2},{3})", X, Y, Z, W);
+        }
+        
+        public double[] ToArray()
+        {
+            return new[] { X, Y, Z, W};
+        }
+
     }
 }
